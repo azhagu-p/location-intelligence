94cc73e89da342c092aa66aa879cdf6a4c33c016
azhagu379 <a.palanisamy@memob.com>
2022-12-02 15:58:33 +0530

    folder structure

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,42 +1,44 @@
-import {useRoutes} from "react-router-dom";
+import { useRoutes } from "react-router-dom";
 import Home from "../pages/HomePage";
-import Login from "../pages/LoginPage";
-import Forgotpassword from "../pages/forgotpassword"
-import Setnewpassword from "../pages/setnewpassword"
+import Login from "../pages/auth/LoginPage";
+import Forgotpassword from "../pages/auth/forgotpassword";
+import Setnewpassword from "../pages/auth/setnewpassword";
 import Product from "../pages/Products";
 import Dashboard from "../pages/Dashboard";
 import React from "react";
 import SelectedCountry from "../pages/SelectedCountry";
 
-const UseRout = () => { return  useRoutes([
-  {
-        path: "/country",
-        element: <SelectedCountry/>,
+const UseRout = () => {
+  return useRoutes([
+    {
+      path: "/country",
+      element: <SelectedCountry />,
     },
     {
-        path: "/",
-        element: <Login/>
+      path: "/",
+      element: <Login />,
     },
     {
-        path:"/forgotpassword",
-        element: <Forgotpassword/>
+      path: "/forgotpassword",
+      element: <Forgotpassword />,
     },
     {
-        path:"/setnewpassword",
-        element: <Setnewpassword/>
+      path: "/setnewpassword",
+      element: <Setnewpassword />,
     },
     {
-        path: "/products",
-        element: <Product/>
+      path: "/products",
+      element: <Product />,
     },
     {
-        path: "/dashboard",
-        element: <Dashboard/>
+      path: "/dashboard",
+      element: <Dashboard />,
     },
     {
-        path: "/selectcountry",
-        element: <SelectedCountry/>
-    }
-])};
+      path: "/selectcountry",
+      element: <SelectedCountry />,
+    },
+  ]);
+};
 
 export default UseRout;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,14 @@
 import React, { useContext } from "react";
-import './Home.css';
-import TextAnimation from "../components/animations/TextAnimation";
+import "./Home.css";
 import Header from "../components/Header";
 
 const Home = () => {
-    return (
-        <div className='home'>
-        <Header/>
-                <div className="title"> <TextAnimation/> </div>
-        </div>
-    )
-}
+  return (
+    <div className="home">
+      <Header />
+      {/* <div className="title"> <TextAnimation/> </div> */}
+    </div>
+  );
+};
 
-export default Home
+export default Home;
